2e9e24e81ce45bb5334edeae6a7af4d5aa8fc90a
happou31 <happo31@gmail.com>
2020-06-12 11:40:08 +0900

    add: ReduxとURLのパース

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const app = <div>hello</div>;
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import createAppReducer from "./Reducers/AppStateReducer";
+import IndexContainer from "./Container/IndexContainer";
+
+const locationHref = location.href;
+
+const firstView: string = decodeURIComponent(
+  locationHref.slice(locationHref.indexOf("n=") + 2)
+);
+
+console.log({ locationHref, firstView });
+
+const store = createStore(createAppReducer({ url: firstView }));
+
+const app = (
+  <Provider store={store}>
+    <IndexContainer />
+  </Provider>
+);
 
 ReactDOM.render(app, document.getElementById("app"));
