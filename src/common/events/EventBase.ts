4ca0b00a3640ee9e5990b51695209ecef093b678
happou31 <happo31@gmail.com>
2020-06-16 11:51:40 +0900

    fix: ドキュメント修正

--- src/common/events/EventBase.ts
+++ src/common/events/EventBase.ts
@@ -13,9 +13,9 @@ export type EventType<T> = {
  * // 例
  namespace InitialStateEvent {
     // レンダラーが送信するチャンネル名を定義する
-    export const CHANNEL_NAME_RENDERER = "RENDERER.REQUEST_INITIAL_STATE";
+    export const CHANNEL_NAME_FROM_RENDERER = "RENDERER.REQUEST_INITIAL_STATE";
     // メインプロセスが送信するチャンネル名を定義する
-    export const CHANNEL_NAME_MAIN = "MAIN.RESPONSE_INITIAL_STATE";
+    export const CHANNEL_NAME_FROM_MAIN = "MAIN.RESPONSE_INITIAL_STATE";
     // ipcXXX.send で 第2引数に渡すPayloadの型を定義。EventType<T> を使う
     export type EventPayload = EventType<AppState>;
  }
