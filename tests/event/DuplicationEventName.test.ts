6383a5d39c62c7004cc142295fb2ebda10b3d14a
happou31 <happo31@gmail.com>
2020-06-16 11:49:16 +0900

    add: test を追加

---
+++ tests/event/DuplicationEventName.test.ts
@@ -0,0 +1,30 @@
+import { describe, it } from "mocha";
+import * as chai from "chai";
+import {
+  CHANNEL_NAME_FROM_MAIN as mainStateChangedEventName,
+  CHANNEL_NAME_FROM_RENDERER as rendererStateChangedEventName,
+} from "@events/StateChangedEvent";
+import {
+  CHANNEL_NAME_FROM_MAIN as mainInitialStateEventName,
+  CHANNEL_NAME_FROM_RENDERER as rendererInitialStateEventName,
+} from "@events/InitialStateEvent";
+
+const eventNameList = [
+  mainStateChangedEventName,
+  rendererStateChangedEventName,
+  mainInitialStateEventName,
+  rendererInitialStateEventName,
+];
+
+describe(__filename, () => {
+  it("イベント名に重複がないかをチェケラ", () => {
+    eventNameList.reduce<string[]>((prev, current) => {
+      chai.assert.isFalse(
+        prev.some((eventName) => current === eventName),
+        `EventNameが重複: ${current} in [${prev.join(", ")}]`
+      );
+
+      return [...prev, current];
+    }, []);
+  });
+});
