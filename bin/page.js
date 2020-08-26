f2c61ef2a0cdfce2b8aa8170d1a3d87299bdea62
happou31 <happo31@gmail.com>
2020-08-27 00:24:23 +0900

    fix: case sensitive

--- bin/page.js
+++ bin/page.js
@@ -2,7 +2,7 @@ const marked = require("marked");
 const fs = require("fs").promises;
 
 (async () => {
-  const buffer = await fs.readFile("./Readme.md");
+  const buffer = await fs.readFile("./README.MD");
   const text = await buffer.toString("utf-8");
   const html = marked(text);
   await fs.writeFile("./index.html", html);
